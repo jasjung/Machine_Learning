917b43dd0162c53b39060bc0ba9c6ca7f1e652e6
Jason Jung <insikjung2017@u.northwestern.edu>
2018-04-11 10:16:56 -0700

    update

--- GitHub/Automatic_Push_Pull/git_pull.sh
+++ GitHub/Automatic_Push_Pull/git_pull.sh
@@ -1,6 +1,11 @@
 
 # update my github folder 
 
+echo
+echo '************************************************'
+echo 'Pulling ...'
+echo '************************************************'
+
 cd /Users/[your_user_name]/Desktop/Github/Python && git pull origin master 
 
 
--- GitHub/Automatic_Push_Pull/git_push.sh
+++ GitHub/Automatic_Push_Pull/git_push.sh
@@ -1,5 +1,10 @@
 
 # update my github folder 
 
+echo
+echo '************************************************'
+echo 'Pushing ...'
+echo '************************************************'
+
 cd /Users/[your_user_name]/Desktop/Github/Python && git add . && git commit -m 'update' && git push origin master
 
