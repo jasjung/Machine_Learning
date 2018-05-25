4378be53dafa88fac03337b16575eea6759a790d
Jason Jung <insikjung2017@u.northwestern.edu>
2018-05-25 15:30:03 -0700

    add command line argument tutorial

---
+++ *Basics/Command_Line_Argument/command_line_argument.py
@@ -0,0 +1,24 @@
+import sys
+
+# check if it has any arguments
+
+print()
+arg_num = len(sys.argv) 
+
+if arg_num == 1: 
+	print('there are no arguments')
+
+elif arg_num > 1: 
+	# subtract 1 because one of the arguments is the file name 
+	print('there are %d arguments ' % (arg_num-1))
+	print(sys.argv)
+
+	# how to combine all arguments without quotations 
+	one_string = " ".join(sys.argv[1:])
+	print(one_string)
+	print(type(one_string))
+
+print('done')
+print()
+
+
